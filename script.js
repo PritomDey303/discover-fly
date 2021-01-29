715a1d88f3a66c7ee62e95f7d0bb392afc73f1c9
Pritom Dey <pritomdey4@gmail.com>
2021-01-30 00:42:41 +0600

    third-commit

--- script.js
+++ script.js
@@ -43,28 +43,44 @@ function ticketCount(binary,seat){
             }
         }
     }   
-    totalAmount();
 
+    //calling totalAmount Function
+    
+    totalAmount();
 }
+
+
+
 
 
 //totalAmount Function
 
 function totalAmount(){
-    console.log("Called");
+   
     //Declaring necessary variables
 
-    let first_ticket=document.getElementById("first_class").value;
-    first_ticket=parseFloat(first_ticket);
+    let first_class=document.getElementById("first_class");
+    first_ticket=parseFloat(first_class.value);
 
-    let economic_ticket=document.getElementById("economy_class").value;
-    economic_ticket=parseFloat(economic_ticket);
+    let economic_class=document.getElementById("economy_class");
+    economic_ticket=parseFloat(economic_class.value);
 
     let subTotal=document.getElementById("subtotal");
 
     let vat=document.getElementById("vat");
 
     let amount=document.getElementById("amount");
+
+    //checking whether input fields contains negative value
+    if(first_ticket<0){
+        first_ticket=0;
+        first_class.value=0;
+        
+    }
+    if(economic_ticket<0){
+        economic_ticket=0;
+        economic_class.value=0;
+    }
     
     //Calculating subtotal
     let tempSubtotal=first_ticket*150+economic_ticket*100;
@@ -78,4 +94,43 @@ function totalAmount(){
     let tempAmount=tempVat+tempSubtotal;
     amount.innerText=`$${tempAmount}`;
 
+}
+
+
+
+//alertMsg function
+
+function alertMsg(){
+    //declaring variables
+    let flyingFrom=document.getElementById("flying_from").value;
+    let destination=document.getElementById("destination").value;
+    let departure=document.getElementById("departure").value;
+    let returning=document.getElementById("return").value;
+    let firstClass=document.getElementById("first_class").value;
+    let economyClass=document.getElementById("economy_class").value;
+
+    //checking empty or invalid values
+
+    if(flyingFrom==""){
+        alert("Please enter Flying From option.");
+    }
+    else if(destination==""){
+        alert("Please enter Flying To option.");
+    }
+    else if(departure==""){
+        alert("Please enter departure To option.");
+    }
+    else if(returning==""){
+        alert("Please enter return To option.");
+    }
+    else if(firstClass==0 && economyClass==0){
+        alert("You have not booked any ticket.");
+    }
+    else{
+        
+        alert("Congratulation! You have successfully purchased tickets.");
+    }
+    
+    
+    
 }
